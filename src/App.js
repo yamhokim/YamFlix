d02fe93b06ec3a7cd4e8c358b03097b4db3984ac
yamhokim <yoonho.luke.kim@gmail.com>
2024-07-22 16:22:39 -0400

    created a custom hook to handle the localStorage functionality and a potential fix for the manifest error

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import { queryByTestId } from "@testing-library/react";
 import { useCallback, useEffect, useRef, useState } from "react";
 import StarRating from "./StarRating";
 import { useMovies } from "./useMovies";
+import { useLocalStorageState } from "./useLocalStorageState";
 
 const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
@@ -14,10 +14,7 @@ export default function App() {
   const handleCloseMovie = useCallback(() => setSelectedId(null), []);
   const { movies, isLoading, error } = useMovies(query, handleCloseMovie);
 
-  const [watched, setWatched] = useState(function () {
-    const storedValue = localStorage.getItem("watched");
-    return JSON.parse(storedValue) || [];
-  });
+  const [watched, setWatched] = useLocalStorageState([], "watched");
 
   function handleSelectMovie(id) {
     setSelectedId((selectedId) => (id === selectedId ? null : id));
@@ -32,13 +29,6 @@ export default function App() {
   function handleDeleteWatched(id) {
     setWatched((watched) => watched.filter((movie) => movie.imdbID !== id));
   }
-
-  useEffect(
-    function () {
-      localStorage.setItem("watched", JSON.stringify(watched));
-    },
-    [watched]
-  );
 
   return (
     <>
